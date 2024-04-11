42cc7c541cd7d68d33b62cc2bf6588476245dc69
Anita <andrasova42@gmail.com>
2024-04-11 19:36:26 +0200

    adjusted greeting

---
+++ CSharp2_2024/Lesson1_Revision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson1_Revision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Rosenval!");
+        }
+    }
+}
